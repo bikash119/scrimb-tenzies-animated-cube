efadf2173d1e39da96433904ec84e1943d80f577
bikash119 <bikashjpatra@gmail.com>
2025-03-30 08:21:36 +0530

    randomized background color of each side for each die

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,27 @@ import Die from './components/Die'
 import { useState,useEffect } from 'react'
 
 function App() {
-  const [dice,setDice] = useState<{id:string,isHeld:boolean}[]>([])
+  const fillDice = () => {
+    return Array(10).fill(0).map(() => ({id:nanoid(),isHeld:false}))
+  }
+  const [dice,setDice] = useState<{id:string,isHeld:boolean}[]>(() => fillDice())
   const [rolling,setRolling] = useState(false)
-  const die = Array(10).fill(0).map((_,index) => ({id:nanoid(),isHeld:false}))
   const rollDice = () => {
     setDice(prevDice => prevDice.map(die => die.isHeld ? die : {...die,value:Math.ceil(Math.random() * 6)}))
     setRolling(!rolling)
+  }
+  const holdDie = (id:string) => {
+    setDice(prevDice => prevDice.map(die => die.id === id? {...die,isHeld:!die.isHeld}:die))
   }
   useEffect(() => {
     document.querySelectorAll('.die-3d-obj')?.forEach(die => {
       die.classList.add('die-3d-obj-rolling')
     })
+    setTimeout(() => {
+      document.querySelectorAll('.die-3d-obj')?.forEach(die => {
+        die.classList.remove('die-3d-obj-rolling')
+      })
+    }, 8000)
   },[rolling])
  
   return (
@@ -25,8 +35,8 @@ function App() {
         </section>
         <section className='container'>
           <div className='dice-container'>
-            {die.map((eachDie,index) => (
-              <Die key={eachDie.id} isHeld={eachDie.isHeld}/>
+            {dice.map((eachDie,index) => (
+              <Die key={eachDie.id} isHeld={eachDie.isHeld} onClick={() => holdDie(eachDie.id)}/>
             ))}
           </div>
         </section>
--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 export default function Die(props:any){
     const sides:string[] = ["front","back","right","left","top","bottom" ]
     const colors:string[] = ["red","blue","green","brown","orange","purple"]
-    const randomDips = () => {
+    const randomDips = (() => {
         const newArray:number[] = []
         while(newArray.length < sides.length){
             const randomNumber = Math.ceil(Math.random() * sides.length)
@@ -13,19 +13,27 @@ export default function Die(props:any){
             }
         }
         return newArray;
-    }
-    const faceValues = randomDips()
-    const colorScheme = colors.map(() => colors[Math.floor(Math.random() * 6)])
-    const sidesWithDips= ()  =>  sides.map((eachSide,index) => ({id:nanoid(), side:eachSide,value:faceValues[index]}))
+    })();
+    const colorScheme = (() => {
+        const newArray:string[] = []
+        while(newArray.length < sides.length){
+            const randomNumber = Math.ceil(Math.random() * sides.length)
+            if(!newArray.includes(colors[randomNumber-1])){
+                newArray.push(colors[randomNumber-1])
+            }
+        }
+        return newArray;
+    })();
+    const sidesWithDips= ()  =>  sides.map((eachSide,index) => ({id:nanoid(), side:eachSide,value:randomDips[index]}))
     return(
-        <div className="die">
+        <button className="die" onClick={props.onClick}>
             <div className="die-3d-space"> {/* Container to house the 3d Space */}
                 <div className="die-3d-obj"> {/* 3D Object */}
                 {sidesWithDips().map((eachSide:any,index:number) => (
-                    <Dieface key={eachSide.id} side={eachSide.side} value={eachSide.value} backgroundColor={colorScheme[index]} />
+                    <Dieface key={eachSide.id} side={eachSide.side} value={eachSide.value} backgroundColor={colorScheme[index]} /> 
                 ))}
                 </div>
             </div>
-        </div>
+        </button>
     )
 }
